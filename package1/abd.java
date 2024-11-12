e72d918cbf2ebc0e433c2e1e6798ad2a380a38f8
abdul rahman <rahman-abd>
2024-11-12 11:44:41 +0530

    add new file

--- package1/abd.java
+++ package1/abd.java
@@ -3,7 +3,7 @@ package package1;
 public class abd {
     public static void main(String[] args)
     {
-        System.out.println("hi hello");
+        System.out.println("hi hello welcome");
     }
     
 }
