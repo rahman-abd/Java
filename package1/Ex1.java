f03abea77e27124ecc9f187a81e6b94ecb23f5a6
rahman-abd <astarabdul007@gmail.com>
2024-11-15 18:43:26 +0530

    new added

---
+++ package1/Ex1.java
@@ -0,0 +1,21 @@
+package package1;
+
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner (System.in);
+        System.out.println("Enter the value: ");
+        int num=s.nextInt();
+        s.close();
+        int ans=0;
+        while (num>0) {
+            int rem= num%10;
+            ans=ans*10+rem;
+            num/=10;
+            
+        }
+        System.out.println("ans: "+ans);
+    }
+    
+}
